0886eb202e652d9110ea1921fa22da38fb4187fe
Ryan Schalk <schalkr1@mymail.nku.edu>
2018-04-08 05:26:56 -0400

    Initial commit.

---
+++ config.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='WikiGroup7'
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+CONTENT_DIR = '/Users/Ryan/PyCharmProjects/WikiGroup7/content'
+USER_DIR = '/Users/Ryan/PyCharmProjects/WikiGroup7/user'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
